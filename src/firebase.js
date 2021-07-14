9d0f585fb28c9daa31a28086898c449b0ea6ed47
Arecio Canton <cuba289@gmail.com>
2021-07-14 01:35:55 -0400

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBWcAGzqaCWoTJZxMu2z0gJgs4InD2O1dI",
+    authDomain: "nozama-53fd6.firebaseapp.com",
+    projectId: "nozama-53fd6",
+    storageBucket: "nozama-53fd6.appspot.com",
+    messagingSenderId: "614881357118",
+    appId: "1:614881357118:web:95d506288a40cd8b5fd06f",
+    measurementId: "G-LRB2BTFLFE"
+  };
+  
